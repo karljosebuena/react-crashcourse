9f0d54a72690d347c8bce52887378cc589f436f5
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-16 06:55:49 +0800

    Created first state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completd: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completd: false
+      },
+      {
+        id: 3,
+        title: 'Band Practice',
+        completd: false
+      },
+      {
+        id: 4,
+        title: 'Basketball drills/workout',
+        completd: false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     )
   }
