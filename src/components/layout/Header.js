153fdb074d3c4ba84917c8954f47a12a69ddc28d
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-22 07:06:35 +0800

    Added header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <h1 style={headerStyle}>TodoList</h1>
+            </header>
+        </div>
+    )
+}
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+}
