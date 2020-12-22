463cdcef62337a15655b3afeee5d63cfc18fcbec
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-22 08:17:29 +0800

    Intro on how to add links in react component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
-      <header>
-        <h1 style={headerStyle}>TodoList</h1>
+      <header style={headerStyle}>
+        <h1>TodoList</h1>
+        <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
       </header>
     </div>
   )
@@ -15,4 +17,9 @@ const headerStyle = {
   color: '#fff',
   textAlign: 'center',
   padding: '10px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
 }
